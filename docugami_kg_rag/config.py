a4f59d20c289f7055110171e9b13a63119bb6b25
Taqi Jaffri <tjaffri@docugami.com>
2023-12-06 02:05:52 -0800

    Eval notebook

--- docugami_kg_rag/config.py
+++ docugami_kg_rag/config.py
@@ -72,8 +72,8 @@ class LocalIndexState:
 
 # Lengths for the loader are in terms of characters, 1 token ~= 4 chars in English
 # Reference: https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
-MAX_CHUNK_TEXT_LENGTH = 1024 * 24  # ~6k tokens
-MIN_CHUNK_TEXT_LENGTH = 1024 * 8  # ~2k tokens
+MAX_CHUNK_TEXT_LENGTH = 1024 * 20  # ~5k tokens
+MIN_CHUNK_TEXT_LENGTH = 1024 * 8  # ~1k tokens
 SUB_CHUNK_TABLES = False
 INCLUDE_XML_TAGS = True
 PARENT_HIERARCHY_LEVELS = 1
