885692a030f27865f734c89ad4a220831e845ab0
Taqi Jaffri <tjaffri@docugami.com>
2023-12-06 14:11:38 -0800

    Eval related tweaks

--- docugami_kg_rag/helpers/prompts.py
+++ docugami_kg_rag/helpers/prompts.py
@@ -9,14 +9,21 @@ You ALWAYS follow the following guidance to generate your answers, regardless of
 
 All your answers must contain citations to help the user understand how you created the citation, specifically:
 
-- If the given context contains the names of document(s), make sure you include that in your answer as 
-  a citation, e.g. include "\\n\\nSOURCE(S): foo.pdf, bar.pdf" at the end of your answer.
+- If the given context contains the names of document(s), make sure you include the document you got the
+  answer from as a citation, e.g. include "\\n\\nSOURCE(S): foo.pdf, bar.pdf" at the end of your answer.
 - If the answer was generated via a SQL Query, make sure you include the SQL query in your answer as
   a citation, e.g. include "\\n\\nSOURCE(S): SELECT AVG('square footage') from Leases". The SQL query should be
-  in the agent scratchpad provided.
+  in the agent scratchpad provided, if you are using an agent.
 - Make sure there an actual answer if you show a SOURCE citation, i.e. make sure you don't show only
   a bare citation with no actual answer. 
 
+"""
+
+HUMAN_MESSAGE_TEMPLATE = """{context}
+
+Using the context above, which can include text and tables, answer the following question.
+
+Question: {question}
 """
 
 CREATE_DIRECT_RETRIEVAL_TOOL_DESCRIPTION_PROMPT = """Here is a snippet from a sample document of type {docset_name}:
