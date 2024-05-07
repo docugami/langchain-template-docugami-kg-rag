2e73f32a67b5905814edc18b398f6468e3b5de48
Taqi Jaffri <tjaffri@docugami.com>
2024-05-07 15:26:16 -0700

    lint fixes

--- docugami_kg_rag/config/__init__.py
+++ docugami_kg_rag/config/__init__.py
@@ -23,32 +23,26 @@ if not DOCUGAMI_API_KEY:
     raise Exception("Please set the DOCUGAMI_API_KEY environment variable")
 
 
-INDEXING_LOCAL_STATE_PATH = os.environ.get(
-    "INDEXING_LOCAL_STATE_PATH", "/tmp/docugami/indexing_local_state.pkl"
-)
+INDEXING_LOCAL_STATE_PATH = os.environ.get("INDEXING_LOCAL_STATE_PATH", "/tmp/docugami/indexing_local_state.pkl")
 os.makedirs(Path(INDEXING_LOCAL_STATE_PATH).parent, exist_ok=True)
 
-INDEXING_LOCAL_REPORT_DBS_ROOT = os.environ.get(
-    "INDEXING_LOCAL_REPORT_DBS_ROOT", "/tmp/docugami/report_dbs"
-)
+INDEXING_LOCAL_REPORT_DBS_ROOT = os.environ.get("INDEXING_LOCAL_REPORT_DBS_ROOT", "/tmp/docugami/report_dbs")
 os.makedirs(Path(INDEXING_LOCAL_REPORT_DBS_ROOT).parent, exist_ok=True)
 
 LOCAL_LLM_CACHE_DIR = os.environ.get("LOCAL_LLM_CACHE", "/tmp/docugami/langchain_cache")
 os.makedirs(Path(LOCAL_LLM_CACHE_DIR).parent, exist_ok=True)
 
 
-def get_hashed_name(name):
+def get_hashed_name(name: str) -> str:
     return hashlib.sha256(name.encode()).hexdigest()
 
 
-def init_gptcache(cache_obj: Cache, llm: str):
+def init_gptcache(cache_obj: Cache, llm: str) -> None:
     hashed_llm = get_hashed_name(llm)
     hashed_llm_dir = Path(LOCAL_LLM_CACHE_DIR) / hashed_llm
     cache_obj.init(
         pre_embedding_func=get_prompt,
-        data_manager=manager_factory(
-            manager="map", data_dir=str(hashed_llm_dir.absolute())
-        ),
+        data_manager=manager_factory(manager="map", data_dir=str(hashed_llm_dir.absolute())),
     )
 
 
